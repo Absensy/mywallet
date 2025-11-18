b1cef477b925ce4fcc2cde2126374acc43717b19
Evgeniy <evgenniydasshkevich@gmail.com>
2025-11-18 16:33:45 +0300

    Initial commit

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,14 @@
+import { Box, Container, Typography } from "@mui/material";
+
+
+const Header = () => {
+    return (
+        <Container>
+            <Box>
+                <Typography variant="h6">MyWallet</Typography>
+            </Box>
+        </Container>
+    )
+}
+
+export default Header;
